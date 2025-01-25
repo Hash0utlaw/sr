6d3ded4aabaee1490f461ad5be9d3f091b26d1b9
Hash0utlaw <misfitderbysociety@gmail.com>
2025-01-25 17:37:31 -0500

    fixed contact

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -68,7 +68,7 @@ export function NavBar() {
             className="hidden lg:flex items-center bg-orange-500 text-white px-4 py-2 rounded-full hover:bg-orange-600 transition-colors duration-200 font-bold"
           >
             <Phone size={18} className="mr-2" />
-            <span>(704) 578-4756</span>
+            <span>Contact Us</span>
           </a>
 
           {/* Mobile Menu Button */}
