cdf4e02da0f6bd04d2fd281b67873b6158412b26
Hash0utlaw <misfitderbysociety@gmail.com>
2025-02-13 20:46:53 -0500

    added GTM for the marketing team

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { CookieConsent } from '@/components/CookieConsent';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PNFDMTJT');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PNFDMTJT"
+            height="0" 
+            width="0" 
+            className="hidden-iframe"
+          />
+        </noscript>
         <StrictMode>
           {children}
           <CookieConsent></CookieConsent>
@@ -29,4 +50,3 @@ export default function RootLayout({
     </html>
   );
 }
-
